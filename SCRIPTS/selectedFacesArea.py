45dee95cf2a7e6bc071d632de6c1eca9483cef48
Piotr Zdzieblowski <piotr@zdzieblowski.com>
2020-08-09 04:47:54 +0200

    selectedFacesArea
    1.1

--- SCRIPTS/selectedFacesArea.py
+++ SCRIPTS/selectedFacesArea.py
@@ -4,35 +4,41 @@
 import bpy
 import bmesh
 
+area_sum = 0
+face_count = 0
+result = []
+error = ""
+
+def resultPopup(self, context):
+    self.layout.row().label(text = result[0])
+    self.layout.row().label(text = result[1])
+    self.layout.separator()
+    self.layout.row().label(text = result[2])
+
+def errorPopup(self, context):
+    self.layout.label(text = error)
+
 if bpy.context.edit_object:
 
     mesh = bpy.context.edit_object.data
     bm = bmesh.from_edit_mesh(mesh)
-
-    area_sum = 0
-    face_count = 0
 
     for face in bm.faces:
         if face.select:
             area_sum += face.calc_area()
             face_count += 1
 
-    output = ["SELECTED FACES: %s" % face_count,
+    result = ["SELECTED FACES: %s" % face_count,
     "AVERAGE FACE AREA: %s m2" % (area_sum/face_count if face_count > 0 else 0),
     "TOTAL SELECTED AREA: %s m2" % area_sum]
 
-    def popup(self, context):
-        row = self.layout.row()
-        row2 = self.layout.row()
-        row3 = self.layout.row()
-        row.label(text = output[0])
-        row2.label(text = output[1])
-        row3.label(text = output[2])
+    bpy.context.window_manager.popup_menu(resultPopup, title="[e42] selectedFacesArea", icon='INFO')
 
-    bpy.context.window_manager.popup_menu(popup, title="[e42] selectedFacesArea", icon='INFO')
-
-    print("\n[> e42] selectedFacesArea:\n\n%s\n%s\n\n%s\n\n[<]" % (output[0], output[1], output[2]))
+    print("\n[> e42] selectedFacesArea:\n\n%s\n%s\n\n%s\n\n[<]" % (result[0], result[1], result[2]))
 
 else:
+    error = "ERROR: This script only works in 'Edit Mode'."
 
-    print("\n[> e42] selectedFacesArea:\n\nERROR: This script only works in 'Edit Mode'.\n\n[<]")
+    bpy.context.window_manager.popup_menu(errorPopup, title="[e42] selectedFacesArea", icon='ERROR')
+
+    print("\n[> e42] selectedFacesArea:\n\n%s\n\n[<]" % error)
